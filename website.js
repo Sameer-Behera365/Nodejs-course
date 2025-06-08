bbaf5e8f96348033aab666e3a0abec2a81510eae
Sameer-Behera365 <sonubehera2024@gmail.com>
2025-06-08 20:32:15 +0530

    finalized changes

--- website.js
+++ website.js
@@ -3,43 +3,7 @@
 /*
 
 
-
-If you open:
-🔗 http://localhost:3000/ → You see the Home Page
-
-If you open:
-🔗 http://localhost:3000/About → You see the About Page
-
-If you open:
-🔗 http://localhost:3000/Link → You see the Links Page
-
-If you open anything else (like /xyz):
-❌ You see a 404 Not Found Page
-
-
-
-==========================================================================================
-
-📌 In Simple Words:   use of backticks(``)
-Symbol                 	Can write in many lines?	      Can use variables like ${name}?
-' or " (quotes)	               ❌ No	                     ❌ No
-` ` (backticks)	               ✅ Yes	                 ✅ Yes
-
-
-
-===========================================================================================
-
-
-🔍 What is req.url?  (http://localhost:3000/)
-req.url returns the path part of the URL that comes after the domain and port.
-
-It does NOT include the protocol (http://) or hostname (localhost), or port.
-
-It is  simply the  path the user is requesting on your server.
-
-*/
-
-
+code:-----------------------------------------------------------------------------------------------------------
 
 
 
@@ -100,3 +64,104 @@ server.listen(port, () => {
 });
 
 
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+
+In this project if you open:
+🔗 http://localhost:3000/ → You see the Home Page
+
+If you open:
+🔗 http://localhost:3000/About → You see the About Page
+
+If you open:
+🔗 http://localhost:3000/Link → You see the Links Page
+
+If you open anything else (like /xyz):
+❌ You see a 404 Not Found Page
+
+
+
+==========================================================================================
+
+📌 In Simple Words:    use of backticks(``)
+Symbol                 	Can write in many lines?	      Can use variables like ${name}?
+' or " (quotes)	               ❌ No	                     ❌ No
+` ` (backticks)	               ✅ Yes	                 ✅ Yes
+
+
+
+===========================================================================================
+
+
+🔍 What is req.url?  (http://localhost:3000/)
+req.url returns the path part of the URL that comes after the domain and port.
+
+It does NOT include the protocol (http://) or hostname (localhost), or port.
+
+It is  simply the  path the user is requesting on your server.
+
+========================================================================================================================================================================
+
+*/
+
+
+
+
+
+
+/// now this is basically the same project but we dont use hard coded values instead we use the data from other files 
+
+
+
+const fs = require("fs");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  res.setHeader("Content-type", "text/html");
+
+  if (req.url === "/") 
+  {
+    res.statusCode = 200; //ok
+    res.end(
+      "<h1> This is our http video </h1> <p> Hey this course is usefull! </p>  "
+    );
+  } 
+  
+  
+  
+  else if (req.url == "/About") 
+  {
+    res.statusCode = 200;
+    const data = fs.readFileSync("index.html");
+    res.end(data.toString());
+  } 
+  
+  
+  
+  else if (req.url == "/Link") 
+  {
+    res.statusCode = 200;
+    const data = fs.readFileSync("Link.html");
+    res.end(data.toString());
+  } 
+  
+  
+  
+  else 
+  {
+    // res.calljn();
+    res.statusCode = 404;
+    res.end("<h1> 404 Not Found </h1>  ");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port} `);
+});
